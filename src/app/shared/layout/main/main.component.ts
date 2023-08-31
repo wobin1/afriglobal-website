1116e7cebf7fbb34b44735d207bcec7d6c3eb20b
wobin1 <nathanielmusa3@gmail.com>
2023-08-31 09:20:14 +0100

    profile downloadable

--- src/app/shared/layout/main/main.component.ts
+++ src/app/shared/layout/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoutingService } from '../../services/routing-service/routing.service';
 
 @Component({
   selector: 'app-main',
@@ -9,11 +10,18 @@ export class MainComponent {
 
   sidenav:boolean=false;
 
+  constructor(private router: RoutingService){}
+
   toggle(){
     this.sidenav = !this.sidenav;
   }
 
   closeToggle(){
+    this.sidenav = false;
+  }
+
+  route(page:string){
+    this.router.route(page)
     this.sidenav = false;
   }
 
